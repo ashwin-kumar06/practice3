18ab5e0adfdd795097e65cf92288d5a1308f79de
ashwin <ashwinkumar0850@gmail.com>
2023-09-12 22:16:25 +0530

    first

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿public class Methods{
+    public void Kavin(){
+
+    }
+    public void Keerthi(){
+        
+    }
+}
+
+class Program(){
+    public static void Main(string[] args){
+
+    }
+}
