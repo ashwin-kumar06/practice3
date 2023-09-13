bed75ea28771c5ffbe9cc69fada987b3ba7bbe6e
Your Name <you@example.com>
2023-09-13 07:42:58 +0530

    ll

--- Program.cs
+++ Program.cs
@@ -1,7 +1,10 @@
-﻿public class Methods{
+﻿using System.Diagnostics;
+
+public class Methods{
     public void Kavin(){
         Console.WriteLine("this is kavinpraksh");//change by kavin
         Console.WriteLine("second update");//kavin
+        Console.WriteLine("kik");
     }
     public void Keerthi(){
         Console.WriteLine("this is vasan");
